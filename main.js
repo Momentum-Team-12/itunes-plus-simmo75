94c9c05668aec245cc3ca1f6e9d253db3c965bae
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-08 14:19:27 -0700

    addedfontsandsomecss

--- main.js
+++ main.js
@@ -5,39 +5,29 @@ let profileDiv = document.querySelector('#profile')
 fetch('https://itunes.apple.com/search?term=jdilla&media=music', {
   method: 'GET',
 })
-.then(function (response) {
+  .then(function (response) {
     return response.json()
-})
-.then (function (data) {
-// for (let artist of data) {
-    // console.log('artists')
-    // let nameDiv = document.createElement('p')
-    // nameDiv.classList.add('artist')
-    // // nameDiv.innerHTML = '<a href="' + i.html_url + '">' + i.artistName + "</a>" 
-    //   nameDiv.innerText = artist.artistName  
-    // profileDiv.appendChild(nameDiv)
+  })
+  .then(function (data) {
     console.log(data.results)
-//  let nameDiv = document.createElement('p')  
-//  nameDiv.classList.add('artist')
-//  nameDiv.innerText = results.artistName 
-//  profileDiv.appendChild(nameDiv)
-// }
+  
+    // }
 
-let results = data.results.slice(1)
-for (let result of results) {
-let nameDiv = document.createElement('p')  
- nameDiv.classList.add('artist')
- nameDiv.innerText = result.artistName 
- profileDiv.appendChild(nameDiv)
+    let results = data.results.slice(1)
+    for (let result of results) {
+      let nameDiv = document.createElement('p')
+      nameDiv.classList.add('artist')
+      nameDiv.innerText = result.artistName
+      profileDiv.appendChild(nameDiv)
 
-let songDiv = document.createElement('p')  
- songDiv.classList.add('song')
- songDiv.innerText = result.trackName 
- profileDiv.appendChild(songDiv)
+      let songDiv = document.createElement('p')
+      songDiv.classList.add('song')
+      songDiv.innerText = result.trackName
+      profileDiv.appendChild(songDiv)
 
-let artDiv = document.createElement('img')  
- artDiv.classList.add('song')
- artDiv.src = result.artworkUrl100 
- profileDiv.appendChild(artDiv)
-}
-})
+      let artDiv = document.createElement('img')
+      artDiv.classList.add('song')
+      artDiv.src = result.artworkUrl100
+      profileDiv.appendChild(artDiv)
+    }
+  })
