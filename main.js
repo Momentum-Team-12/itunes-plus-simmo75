cde96d26657d3b34a4d21ab612596ad9a2f6a0f4
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-09 14:48:15 -0700

    css-movedartistnameaboveimg

--- main.js
+++ main.js
@@ -10,31 +10,28 @@ fetch('https://proxy-itunes-api.glitch.me/search?term=jdilla&media=music', {
   })
   .then(function (data) {
     console.log(data.results)
-  
+
     // }
 
     let results = data.results.slice(1)
     for (let result of results) {
-
-
       let resultsDiv = document.createElement('div')
       resultsDiv.classList.add('results')
-
 
       let nameDiv = document.createElement('div')
       nameDiv.classList.add('artist')
       nameDiv.innerText = result.artistName
       resultsDiv.appendChild(nameDiv)
 
-      let songDiv = document.createElement('div')
-      songDiv.classList.add('song')
-      songDiv.innerText = result.trackName
-      resultsDiv.appendChild(songDiv)
-
       let artDiv = document.createElement('img')
       artDiv.classList.add('img')
       artDiv.src = result.artworkUrl100
       resultsDiv.appendChild(artDiv)
+
+      let songDiv = document.createElement('div')
+      songDiv.classList.add('song')
+      songDiv.innerText = result.trackName
+      resultsDiv.appendChild(songDiv)
 
       profileDiv.appendChild(resultsDiv)
     }
