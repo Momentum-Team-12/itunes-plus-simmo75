1723e4b32de3371c5a6f7cf139b02946ac155120
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-11 12:51:42 -0700

    searchadded

--- main.js
+++ main.js
@@ -2,60 +2,53 @@ console.log('linked')
 
 let profileDiv = document.querySelector('#profile')
 
-fetch('https://proxy-itunes-api.glitch.me/search?term=jdilla&media=music', {
+let searchform = document.querySelector('#search')
+
+searchform.addEventListener ('submit', function(event) {
+event.preventDefault()
+
+let userInput = document.querySelector('#user-input')
+console.log (userInput.value)
+
+
+
+fetch(`https://proxy-itunes-api.glitch.me/search?term=${userInput.value}&media=music`, {
   method: 'GET',
 })
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (data) {
-    console.log(data.results)
-
-    // }
-
-    let results = data.results.slice(1)
-    for (let result of results) {
-      let resultsDiv = document.createElement('div')
-      resultsDiv.classList.add('results')
-
-      let nameDiv = document.createElement('div')
-      nameDiv.classList.add('artist')
-      nameDiv.innerText = result.artistName
-      resultsDiv.appendChild(nameDiv)
-
-      let artDiv = document.createElement('img')
-      artDiv.classList.add('img')
-      artDiv.src = result.artworkUrl100
-      resultsDiv.appendChild(artDiv)
-
-      let songDiv = document.createElement('div')
-      songDiv.classList.add('song')
-      songDiv.innerText = result.trackName
-      resultsDiv.appendChild(songDiv)
-
-      let releaseDiv = document.createElement('div')
-      releaseDiv.classList.add('release')
-      releaseDiv.innerText = result.releaseDate
-      resultsDiv.appendChild(releaseDiv)
+.then(function (response) {
+  return response.json()
+})
+.then(function (data) {
+  console.log(data.results)
+  
+  
+  
+  let results = data.results.slice(1)
+  for (let result of results) {
+    let resultsDiv = document.createElement('div')
+    resultsDiv.classList.add('results')
     
-      // function releaseDate(year) {
-       
-      //  return year.charAt(0).getFullYear() + year.slice(3);
-      // }
-      // const releaseDate = new Date(document.createElement);
-      // let year = releaseDate.getFullYear();
-
-
+    let nameDiv = document.createElement('div')
+    nameDiv.classList.add('artist')
+    nameDiv.innerText = result.artistName
+    resultsDiv.appendChild(nameDiv)
     
-
-
-      
-      
-      
-      
-      
-      
-
+    let artDiv = document.createElement('img')
+    artDiv.classList.add('img')
+    artDiv.src = result.artworkUrl100
+    resultsDiv.appendChild(artDiv)
+    
+    let songDiv = document.createElement('div')
+    songDiv.classList.add('song')
+    songDiv.innerText = result.trackName
+    resultsDiv.appendChild(songDiv)
+    
+    let releaseDiv = document.createElement('div')
+    releaseDiv.classList.add('release')
+    releaseDiv.innerText = result.releaseDate
+    resultsDiv.appendChild(releaseDiv)
+  
       profileDiv.appendChild(resultsDiv)
-    }
+  } 
   })
+})
