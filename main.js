f718701d409db1e1434afd8554470b7ce55e8f75
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-04-12 13:43:06 -0700

    truncated-date-to-only-show-year

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ searchform.addEventListener('submit', function (event) {
   console.log(userInput.value)
 
   removeAllChildNodes(profileDiv)
-  //removes child elements, pre search input//
 
   fetch(
     `https://proxy-itunes-api.glitch.me/search?term=${userInput.value}&media=music`,
@@ -27,42 +26,46 @@ searchform.addEventListener('submit', function (event) {
       method: 'GET',
     },
   )
-    //defining the website/database for our search and then//
-
     .then(function (response) {
       return response.json()
     })
+    //defining the website/database for our search and then//
+
     .then(function (data) {
       console.log(data.results)
 
       let results = data.results.slice(1)
       for (let result of results) {
+
         let resultsDiv = document.createElement('div')
         resultsDiv.classList.add('results')
         // this div-type element is the container for all artist info
+        
         let artDiv = document.createElement('img')
         artDiv.classList.add('img')
         artDiv.src = result.artworkUrl100
         resultsDiv.appendChild(artDiv)
         // this div-type element is the container for all album artwork info
+        
         let audioDiv = document.createElement('audio')
         audioDiv.classList.add('audioPreview')
         audioDiv.src = result.previewUrl
         audioDiv.controls = 'controls'
         resultsDiv.appendChild(audioDiv)
-
+        // this div-type element is the container for all audio previews
+        
         let songDiv = document.createElement('div')
         songDiv.classList.add('song')
         songDiv.innerText = result.trackName
         resultsDiv.appendChild(songDiv)
         // this div-type element is the container for all song info
 
+        let releaseDiv = document.createElement('div')
+        releaseDiv.classList.add('release')
+        releaseDiv.innerText = `${moment(result.releaseDate).format("YYYY")}`;
+        resultsDiv.appendChild(releaseDiv)
+        
 
-
-        // let releaseDiv = document.createElement('div')
-        // releaseDiv.classList.add('release')
-        // releaseDiv.innerText = result.releaseDate
-        // resultsDiv.appendChild(releaseDiv)
 
         profileDiv.appendChild(resultsDiv)
         profileDiv.remove
